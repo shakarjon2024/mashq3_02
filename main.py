43c40910ec6e17b1bd769d20e94fd760003129ad
shakarjon2024 <rozibayevashakarjon1707@gmail.com>
2025-12-02 15:41:22 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,66 @@
+# 1 - misol
+num = lambda x: x + 1
+print(num(7))
+
+
+# 2 - misol
+numbers = lambda a, b, c: a if a < b and a < c else (b if b < c else c)
+print(numbers(6, 4, 9))
+print(numbers(2, 7, 4))
+
+
+# 3 - misol
+ism = lambda x:  x.capitalize()
+print(ism('tom'))
+print(ism('alice'))
+
+
+# 4 - misol
+royxat = (2, 7, 4, 9, 3)
+roy = lambda x: len(x)
+print(roy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
